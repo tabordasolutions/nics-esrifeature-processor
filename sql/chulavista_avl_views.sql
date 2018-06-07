02d76c1047fcdc27950e4cb4430ad2b0623770de
snagole <sneha.nagole@tabordasolutions.com>
2018-06-07 16:55:56 -0700

    Changes to montevista avl db view

--- sql/chulavista_avl_views.sql
+++ sql/chulavista_avl_views.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE VIEW public.montevista_units_avl AS
  SELECT gpf.the_geom AS location,
-    gpf.properties::json ->> 'VehicleName'::text AS "Vehicle Name",
+    gpf.properties::json ->> 'VehicleName'::text AS "VehicleName",
     COALESCE(gpf.properties::json ->> 'Heading'::text, '0'::text) AS "Heading",
     COALESCE(gpf.properties::json ->> 'Speed'::text, '0'::text) AS "Speed",
-    COALESCE(to_char(to_timestamp((((gpf.properties::json ->> 'UpdateDateTime_UTC'::text)::numeric) / 1000::numeric)::double precision), 'YYYY/MM/DD HH24:MI:SS'::text), ' '::text) AS "Update Date",
+    COALESCE(to_char(to_timestamp((((gpf.properties::json ->> 'UpdateDateTime_UTC'::text)::numeric) / 1000::numeric)::double precision), 'YYYY/MM/DD HH24:MI:SS'::text), ' '::text) AS "UpdateDate",
     gpf.properties::json ->> 'OBJECTID'::text AS "ObjectID",
     'Point'::text AS "Shape"
    FROM geojson_point_feeds gpf
