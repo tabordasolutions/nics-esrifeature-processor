a19d3565b47d8ab10fc4d5a570c8dac1f75d9764
snagole <sneha.nagole@tabordasolutions.com>
2018-06-14 12:06:07 -0700

    Changes to add more columns to montevista_units_avl view

--- sql/chulavista_avl_views.sql
+++ sql/chulavista_avl_views.sql
@@ -5,7 +5,12 @@ CREATE OR REPLACE VIEW public.montevista_units_avl AS
     COALESCE(gpf.properties::json ->> 'Speed'::text, '0'::text) AS "Speed",
     COALESCE(to_char(to_timestamp((((gpf.properties::json ->> 'UpdateDateTime_UTC'::text)::numeric) / 1000::numeric)::double precision), 'YYYY/MM/DD HH:MI:SS AM'::text), ' '::text) AS "UpdatedAt_UTC",
     gpf.properties::json ->> 'OBJECTID'::text AS "ObjectID",
-    'Point'::text AS "Shape"
+    COALESCE(gpf.properties::json ->> 'UnitLocation_Latitude'::text, 'Unavailable'::text) as "Latitude",
+    COALESCE(gpf.properties::json ->> 'UnitLocation_Longitude'::text, 'Unavailable'::text) as "Longitude",
+    'Point'::text AS "Shape",
+    gpf.properties::json ->> 'VehicleID'::text as "VehicleID",
+    COALESCE(gpf.properties::json ->> 'Description'::text,'Unavailable'::text) as "Description",
+    COALESCE(gpf.properties::json ->> 'JurisdictionID'::text, 'Unavailable'::text) as "JurisdictionID"
    FROM geojson_point_feeds gpf
   WHERE gpf.feedname::text = 'montevista_units_avl'::text
   ORDER BY (gpf.properties::json ->> 'StatusTime'::text) DESC;
