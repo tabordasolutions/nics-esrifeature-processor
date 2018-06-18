cf7c12416ce4961ad6469d1254a30e4a6e3adc14
snagole <sneha.nagole@tabordasolutions.com>
2018-06-18 15:09:09 -0700

    Changes to add new function for heartland communication units

--- sql/chulavista_avl_views.sql
+++ sql/chulavista_avl_views.sql
@@ -14,3 +14,24 @@ CREATE OR REPLACE VIEW public.montevista_units_avl AS
    FROM geojson_point_feeds gpf
   WHERE gpf.feedname::text = 'montevista_units_avl'::text
   ORDER BY (gpf.properties::json ->> 'StatusTime'::text) DESC;
+
+CREATE OR REPLACE VIEW public.heartland_units_avl AS
+ SELECT gpf.the_geom AS location,
+    COALESCE(gpf.properties::json ->> 'JurisdictionID'::text, 'Unavailable'::text) AS "JurisdictionID",
+    gpf.properties::json ->> 'VehicleName'::text AS "VehicleName",
+    gpf.properties::json ->> 'VehicleID'::text AS "VehicleID",
+    COALESCE(gpf.properties::json ->> 'UnitLocation_Latitude'::text, 'Unavailable'::text) AS "Latitude",
+    COALESCE(gpf.properties::json ->> 'UnitLocation_Longitude'::text, 'Unavailable'::text) AS "Longitude",
+    COALESCE(gpf.properties::json ->> 'LocationName'::text, 'Unavailable'::text) AS "Location",
+    COALESCE(gpf.properties::json ->> 'LocationCity'::text, 'Unavailable'::text) AS "City",
+    COALESCE(gpf.properties::json ->> 'Heading'::text, '0'::text) AS "Heading",
+    COALESCE(gpf.properties::json ->> 'Speed'::text, '0'::text) AS "Speed",
+    COALESCE(to_char(to_timestamp((((gpf.properties::json ->> 'UpdateDateTime_UTC'::text)::numeric) / 1000::numeric)::double precision), 'YYYY/MM/DD HH:MI:SS AM'::text), ' '::text) AS "UpdatedAt_UTC",
+    COALESCE(to_char(to_timestamp((((gpf.properties::json ->> 'UpdateDateTime'::text)::numeric) / 1000::numeric)::double precision), 'YYYY/MM/DD HH:MI:SS AM'::text), ' '::text) AS "UpdatedAt_LocalTime",
+    gpf.properties::json ->> 'OBJECTID'::text AS "ObjectID",
+    'Point'::text AS "Shape",
+    gpf.properties::json ->> 'IncidentTrackingID'::text AS "IncidentTrackingID",
+    gpf.properties::json ->> 'AgencyID'::text AS "AgencyID"
+   FROM geojson_point_feeds gpf
+  WHERE gpf.feedname::text = 'heartland_units_avl'::text
+  ORDER BY (gpf.properties::json ->> 'StatusTime'::text) DESC;
