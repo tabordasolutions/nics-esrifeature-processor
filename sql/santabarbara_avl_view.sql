5a9fcd87667cd85c8d3ccf4e475a2dafb99c7844
snagole <sneha.nagole@tabordasolutions.com>
2018-11-12 14:40:59 -0800

    tune santabarabara AVL view to get only records from latest ETL load

--- sql/santabarbara_avl_view.sql
+++ sql/santabarbara_avl_view.sql
@@ -1,10 +1,13 @@
-CREATE OR REPLACE VIEW santabarbara_avl AS
-select gpf.the_geom AS locaiton,
-       coalesce(((gpf.properties)::json ->> 'uuid'::text), ' ') AS "UUID",
-       coalesce(((gpf.properties)::json ->> 'name'::text), ' ')  AS "Name",
-       ((gpf.properties)::json ->> 'short_name'::text) AS "ShortName",
-       ((gpf.properties)::json ->> 'standard_status_code') AS "Standard_Status_Code",
-       ((gpf.properties)::json ->> 'ESRI_OID'::text) AS "ESRI_OID",
-       ((gpf.properties)::json ->> 'group_name'::text) AS "Group_Name",
-       ((gpf.properties)::json ->> 'popupinfo'::text) AS "popupinfo"
-FROM public.geojson_point_feeds gpf where feedname = 'santabarbara_avl';
+CREATE OR REPLACE VIEW public.santabarbara_avl AS
+ SELECT gpf.the_geom,
+    COALESCE(gpf.properties::json ->> 'uuid'::text, ' '::text) AS "UUID",
+    COALESCE(gpf.properties::json ->> 'name'::text, ' '::text) AS "Name",
+    gpf.properties::json ->> 'short_name'::text AS "ShortName",
+    gpf.properties::json ->> 'standard_status_code'::text AS "Standard_Status_Code",
+    gpf.properties::json ->> 'ESRI_OID'::text AS "ESRI_OID",
+    gpf.properties::json ->> 'group_name'::text AS "Group_Name",
+    gpf.properties::json ->> 'popupinfo'::text AS popupinfo
+   FROM geojson_point_feeds gpf
+  WHERE gpf.feedname::text = 'santabarbara_avl'::text AND gpf.created_at = (( SELECT max(geojson_point_feeds.created_at) AS max
+           FROM geojson_point_feeds
+          WHERE geojson_point_feeds.feedname::text = 'santabarbara_avl'::text));
