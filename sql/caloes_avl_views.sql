62e626839843b7dae71820b6cd9f0c89b8c431a2
sneha-nagole <sneha.nagole@tabordasolutions.com>
2018-12-06 10:10:48 -0800

    Update canoes_avl_views.sql
    Update canoes_avl_views.sql to match latest source properties

--- sql/caloes_avl_views.sql
+++ sql/caloes_avl_views.sql
@@ -1,15 +1,15 @@
-CREATE OR REPLACE VIEW public.caloes_vehicles_avl AS
+CREATE OR REPLACE VIEW public.caloes_vehicles_avl AS 
  SELECT gpf.the_geom AS location,
-   gpf.properties::json ->> 'UNIT__'::text AS "UNIT__",
-   gpf.properties::json ->> 'OBJECTID'::text AS "ObjectID",
-       gpf.properties::json ->> 'VIN' AS "VIN",
-       COALESCE(gpf.properties::json ->> 'Heading'::text, '0') AS "Heading",
-       COALESCE(gpf.properties::json ->> 'InstSpeedMPH'::text, '0') AS "InstSpeedMPH",
-       gpf.properties::json ->> 'Unit_Classification'::text AS "UnitClassification",
-       COALESCE(gpf.properties::json ->> 'MessageTimeTxt'::text, 'Unavailable') AS "MessageTimeTxt",
-       gpf.properties::json ->> 'Latitude' AS "Latitude",
-       gpf.properties::json ->> 'Longitude'::text AS "Longitude",
-       'Point'::text AS "Shape"
+    gpf.properties::json ->> 'UNIT__'::text AS "UNIT__",
+    gpf.properties::json ->> 'OBJECTID'::text AS "ObjectID",
+    gpf.properties::json ->> 'VIN'::text AS "VIN",
+    COALESCE(gpf.properties::json ->> 'Heading'::text, '0'::text) AS "Heading",
+    COALESCE(gpf.properties::json ->> 'InstSpeedMPH'::text, '0'::text) AS "InstSpeedMPH",
+    gpf.properties::json ->> 'Unit_Classification'::text AS "UnitClassification",
+    COALESCE(gpf.properties::json ->> 'MessageTimeTxt'::text, 'Unavailable'::text) AS "MessageTimeTxt",
+    gpf.properties::json ->> 'Latitude'::text AS "Latitude",
+    gpf.properties::json ->> 'Longitude'::text AS "Longitude",
+    'Point'::text AS "Shape"
    FROM geojson_point_feeds gpf
-   WHERE gpf.feedname = 'caloes_vehicles_avl'
-   ORDER BY (gpf.properties::json ->> 'MessageTimeTxt'::text) DESC;
+  WHERE gpf.feedname::text = 'caloes_vehicles_avl'::text
+  ORDER BY (gpf.properties::json ->> 'MessageTimeTxt'::text) DESC;
